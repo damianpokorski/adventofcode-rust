1511d1a62c013d075e0bfc8b9c8cb8fc5b1bf0df
Damian Pokorski <damianpokorski@hotmail.com>
2022-10-24 20:10:33 +0100

    2015 Day 6 Part 1

--- src/year2015/day06/mod.rs
+++ src/year2015/day06/mod.rs
@@ -7,9 +7,54 @@ fn read_file() -> String {
   return fs::read_to_string(PATH).expect("Should be able to read the file");
 }
 
-fn part1() -> i32 {
-  read_file();
-  return -1;
+fn part1() -> usize {
+  // Create grid
+  let mut grid = [[false; 1000]; 1000];
+
+  // Apply input
+  for line in read_file().split("\n").map(|x| x ) {
+    let coords: Vec<usize> = line
+      .replace("turn off ", "")
+      .replace("turn on ", "")
+      .replace("toggle ", "")
+      .replace(" through ", ",")
+      .split(",")
+      .map(|word| word.parse().unwrap())
+      .collect();
+
+    let top_x = *coords.get(0).unwrap();
+    let top_y = *coords.get(1).unwrap();
+    let bottom_x = *coords.get(2).unwrap();
+    let bottom_y = *coords.get(3).unwrap();
+  
+    let mut state: Option<bool> = None;
+    if line.starts_with("turn on") {
+      state = Some(true);
+    } else if line.starts_with("turn off") {
+      state = Some(false);
+    }
+
+    for x in top_x..bottom_x+1 {
+      for y in top_y..bottom_y+1 {
+        match state {
+            Some(true) => grid[x][y] = true,
+            Some(false) => grid[x][y] = false,
+            None => grid[x][y] = !grid[x][y],
+        }
+      }
+    }
+  }
+
+  let mut on = 0;
+  for x in 0..1000 {
+    for y in 0..1000 {
+      if grid[x][y] {
+        on +=1;
+      }
+    }
+  }
+  return on;
+
 }
 
 fn part2() -> i32 {
