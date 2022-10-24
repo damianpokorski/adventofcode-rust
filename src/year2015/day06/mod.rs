ecb762e5291165e45394b83c77804f3a12b6394c
Damian Pokorski <damianpokorski@hotmail.com>
2022-10-24 20:16:31 +0100

    2015 Day 6 Complete **

--- src/year2015/day06/mod.rs
+++ src/year2015/day06/mod.rs
@@ -54,12 +54,57 @@ fn part1() -> usize {
     }
   }
   return on;
-
 }
 
-fn part2() -> i32 {
-  read_file();
-  return -1;
+fn part2() -> i64 {
+  // Create grid
+  let mut grid = [[0; 1000]; 1000];
+
+  // Apply input
+  for line in read_file().split("\n").map(|x| x ) {
+    let coords: Vec<usize> = line
+      .replace("turn off ", "")
+      .replace("turn on ", "")
+      .replace("toggle ", "")
+      .replace(" through ", ",")
+      .split(",")
+      .map(|word| word.parse().unwrap())
+      .collect();
+
+    let top_x = *coords.get(0).unwrap();
+    let top_y = *coords.get(1).unwrap();
+    let bottom_x = *coords.get(2).unwrap();
+    let bottom_y = *coords.get(3).unwrap();
+  
+    let mut state: Option<bool> = None;
+    if line.starts_with("turn on") {
+      state = Some(true);
+    } else if line.starts_with("turn off") {
+      state = Some(false);
+    }
+
+    for x in top_x..bottom_x+1 {
+      for y in top_y..bottom_y+1 {
+        match state {
+            Some(true) => grid[x][y] += 1,
+            Some(false) => {
+              if(grid[x][y] >= 1) {
+                grid[x][y] -= 1
+              }
+            },
+            None => grid[x][y] += 2,
+        }
+      }
+    }
+  }
+
+  let mut brightness: i64 = 0;
+  for x in 0..1000 {
+    for y in 0..1000 {
+      brightness += grid[x][y];
+    }
+  }
+  return brightness;
 }
 
 pub fn puzzle() {
