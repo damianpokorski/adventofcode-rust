c89a1e09f798d741de965981b257463786519488
Damian Pokorski <damianpokorski@hotmail.com>
2022-10-24 20:21:49 +0100

    2015 Day 6 - Clean up

--- src/year2015/day06/mod.rs
+++ src/year2015/day06/mod.rs
@@ -7,10 +7,8 @@ fn read_file() -> String {
   return fs::read_to_string(PATH).expect("Should be able to read the file");
 }
 
-fn part1() -> usize {
-  // Create grid
-  let mut grid = [[false; 1000]; 1000];
-
+fn parse() -> Vec<(usize, usize, usize, usize, Option<bool>)> {
+  let mut output:Vec<(usize, usize, usize, usize, Option<bool>)> = Vec::new();
   // Apply input
   for line in read_file().split("\n").map(|x| x ) {
     let coords: Vec<usize> = line
@@ -22,7 +20,7 @@ fn part1() -> usize {
       .map(|word| word.parse().unwrap())
       .collect();
 
-    let top_x = *coords.get(0).unwrap();
+    let top_x= *coords.get(0).unwrap();
     let top_y = *coords.get(1).unwrap();
     let bottom_x = *coords.get(2).unwrap();
     let bottom_y = *coords.get(3).unwrap();
@@ -33,6 +31,19 @@ fn part1() -> usize {
     } else if line.starts_with("turn off") {
       state = Some(false);
     }
+
+    output.push((top_x, top_y, bottom_x, bottom_y, state));
+  }
+  return output;
+}
+
+fn part1() -> usize {
+  // Create grid
+  let mut grid = [[false; 1000]; 1000];
+
+  // Process input
+  for line in parse().into_iter() {
+    let (top_x, top_y, bottom_x, bottom_y, state) = line;
 
     for x in top_x..bottom_x+1 {
       for y in top_y..bottom_y+1 {
@@ -60,29 +71,10 @@ fn part2() -> i64 {
   // Create grid
   let mut grid = [[0; 1000]; 1000];
 
-  // Apply input
-  for line in read_file().split("\n").map(|x| x ) {
-    let coords: Vec<usize> = line
-      .replace("turn off ", "")
-      .replace("turn on ", "")
-      .replace("toggle ", "")
-      .replace(" through ", ",")
-      .split(",")
-      .map(|word| word.parse().unwrap())
-      .collect();
-
-    let top_x = *coords.get(0).unwrap();
-    let top_y = *coords.get(1).unwrap();
-    let bottom_x = *coords.get(2).unwrap();
-    let bottom_y = *coords.get(3).unwrap();
-  
-    let mut state: Option<bool> = None;
-    if line.starts_with("turn on") {
-      state = Some(true);
-    } else if line.starts_with("turn off") {
-      state = Some(false);
-    }
-
+  // Process input
+  for line in parse().into_iter() {
+    let (top_x, top_y, bottom_x, bottom_y, state) = line;
+    
     for x in top_x..bottom_x+1 {
       for y in top_y..bottom_y+1 {
         match state {
