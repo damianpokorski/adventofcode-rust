79dd082a35bc4dc431a69e1db49414ce14eb2e92
Damian Pokorski <damianpokorski@hotmail.com>
2022-11-03 21:23:52 +0000

    2015 Day 9 Still WIP

--- src/year2015/day09/mod.rs
+++ src/year2015/day09/mod.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fs};
+use std::{collections::HashMap, fs, ops::Index};
 
 const PATH: &str = "src/year2015/day09/data.raw";
 
@@ -66,99 +66,113 @@ fn get_node_names(links: &Vec<Link>) -> Vec<String> {
     return unique_names;
 }
 
+fn get_distance(a: &Vec<Link>) -> u32 {
+    if a.len() == 0 {
+        return 0;
+    }
+    if a.len() == 1 {
+        return a.first().unwrap().distance;
+    }
+
+    return a
+        .into_iter()
+        .map(|a| a.distance)
+        .reduce(|a, b| a + b)
+        .unwrap_or(0);
+}
+
 fn propagate(
     from_lookup: &HashMap<String, Vec<Link>>,
     unique_names: &Vec<String>,
     previous_locations: Vec<Link>,
-    current_distance: u32,
-    max_distance: u32,
-) -> Option<(Vec<Link>, u32)> {
-    println!("Unique names: {:?}", unique_names);
+    depth: u32,
+) -> Option<Vec<Link>> {
+    println!("----------");
+    println!("[{:?}] Start", depth);
+    let previous_locations: Vec<Link> = previous_locations.clone();
+    let mut previous_location_names: Vec<String> = vec![];
 
-    let mut previous_locations: Vec<Link> = previous_locations.clone();
+    //
+    let current_location = (&previous_locations).last().unwrap().to.clone();
+    println!("[{:?}] Current location: {:?}", depth, current_location);
 
-    let mut visited_location_names: Vec<String> = (&previous_locations)
-        .into_iter()
-        .map(|location| vec![location.from.clone(), location.to.clone()])
-        .into_iter()
-        .flatten()
-        .collect();
-
-    // Add last stop to the visisted list
-    let current_stop_name = &previous_locations.last().unwrap().to;
-    visited_location_names.push(current_stop_name.clone());
-
-    println!("Visited locations: {:?}", visited_location_names);
-    println!("Current stop: {:?}", current_stop_name);
-
-    // Get available
-    let available_stops = (&from_lookup).get(current_stop_name);
-
-    if available_stops.is_none() {
+    //
+    println!("[{:?}] Path so far: ", depth);
+    for previous_link in (&previous_locations).into_iter() {
+        println!(
+            "[{:?}] - {:?} -> {:?}",
+            depth, previous_link.from, previous_link.to
+        );
+        if !previous_location_names.contains(&previous_link.from) {
+            previous_location_names.push(previous_link.from.clone());
+        }
+        if !previous_location_names.contains(&previous_link.to) {
+            previous_location_names.push(previous_link.to.clone());
+        }
+    }
+    let available_links = (&from_lookup).get(&current_location);
+    if available_links.is_none() {
+        println!("[{:?}] No travel options available: ", depth);
+        // Checking if we have reached our final goal
+        if previous_location_names.len() == unique_names.len() {
+            println!("[{:?}] We have visited all of the places!", depth);
+            return Some(previous_locations.clone());
+        }
         return None;
     }
-    let available_stops = available_stops.unwrap();
-    let targets: Vec<&Link> = (available_stops)
-        .into_iter()
-        .filter(|link| {
-            (&visited_location_names)
-                .into_iter()
-                .any(|visited_location| visited_location != &link.from)
-        })
-        .collect();
 
-    let mut sub_paths: Vec<Option<(Vec<Link>, u32)>> = vec![];
-    let available_location_names: Vec<String> =
-        (&targets).into_iter().map(|link| link.to.clone()).collect();
-
-    println!("Available locations raw raw: {:?}", available_stops);
-    println!("Available locations raw: {:?}", targets);
-    println!("Available locations: {:?}", available_location_names);
-
-    for link in targets {
-        // Add link to the taken locations
-        let mut new_previous_locations = previous_locations.clone();
-        new_previous_locations.push(link.clone());
-
-        // Check if we reached all destinations - this one is a win
-        if !(&unique_names).into_iter().any(|location| {
-            !(&new_previous_locations)
-                .into_iter()
-                .any(|link| &link.to == location || &link.from == location)
-        }) {
-            println!("Victory!");
-            return Some((new_previous_locations, current_distance + link.distance));
+    //
+    println!("[{:?}] Available options: ", depth);
+    for available_link in (&available_links).unwrap().into_iter() {
+        if !previous_location_names.contains(&available_link.to) {
+            println!(
+                "[{:?}] --- {:?} -> {:?}",
+                depth, available_link.from, available_link.to
+            );
         }
-
-        // Check if we reached the max distance - stop propagating
-        if (current_distance + link.distance) > max_distance {
-            println!("End path!");
-            return None;
-        }
-
-        // Keep on digging
-        println!("Digging!");
-        sub_paths.push(propagate(
-            from_lookup,
-            unique_names,
-            new_previous_locations.clone(),
-            (current_distance + link.distance),
-            max_distance,
-        ));
     }
 
-    // Filter invalid paths out
-    let mut sub_paths: Vec<(Vec<Link>, u32)> = sub_paths
-        .into_iter()
-        .filter(|path| path.is_some())
-        .map(|path| path.unwrap())
-        .collect();
+    // Iterate through available links
+    println!("[{:?}] Iterating through links: ", depth);
+    let mut continued_journeys: Vec<Vec<Link>> = vec![];
 
-    // Once we're complete - return the shortest path that's complete
-    if sub_paths.len() > 0 {
-        println!("Sorting!");
-        sub_paths.sort_by(|b, a| a.1.cmp(&b.1));
-        return Some(sub_paths.first().unwrap().clone());
+    for available_link in (&available_links).unwrap().into_iter() {
+        if !previous_location_names.contains(&available_link.to) {
+            println!(
+                "[{:?}] --- {:?} -> {:?}",
+                depth, available_link.from, available_link.to
+            );
+            let mut continued_journey = previous_locations.clone();
+            continued_journey.push(available_link.clone());
+            let result = propagate(from_lookup, unique_names, continued_journey, depth + 1);
+
+            if result.is_some() {
+                continued_journeys.push(result.unwrap().clone());
+            }
+        } else {
+            println!("[{:?}] --- Already visited {:?}", depth, available_link.to);
+        }
+    }
+
+    if continued_journeys.len() > 0 {
+        // Only 1 result - return this
+        if continued_journeys.len() == 1 {
+            return Some(continued_journeys.first().unwrap().clone());
+        }
+
+        // Pick best result out of the multi choice
+        return Some(
+            (&continued_journeys)
+                .into_iter()
+                .reduce(|a, b| {
+                    if get_distance(a) < get_distance(b) {
+                        return a;
+                    }
+                    return b;
+                })
+                .unwrap()
+                .clone(),
+        );
     }
 
     println!("No subpaths returned valid results");
@@ -175,34 +189,37 @@ fn part1() -> usize {
         println!("[{:?}] - {:?}", key, value);
         println!("----");
     }
-    println!("Unique names: {:?}", unique_names);
-
-    // Find max distance covering all links
-    let max_distance = u32::MAX;
 
     println!("----");
     // Quasi A* Path finding?
-    let mut starting_points: Vec<Option<(Vec<Link>, u32)>> = vec![];
+    let mut routes_found: Vec<Vec<Link>> = vec![];
+    let mut loop_index = 0;
     for link in (&results) {
-        starting_points.push(propagate(
-            &from_lookup,
-            &unique_names,
+        println!("----------");
+        println!("Tick {:?}", loop_index);
+        loop_index = loop_index + 1;
+        let result = propagate(
+            &from_lookup.clone(),
+            &unique_names.clone(),
             vec![link.clone()],
-            link.distance,
-            max_distance,
-        ));
-        println!("Tick");
+            0,
+        );
+        if result.is_some() {
+            routes_found.push(result.unwrap().clone());
+        }
     }
-    println!("Hmmm {:?}", starting_points);
-    // Find best starting point
 
-    // for path in &result.unwrap().0 {
-    //     println!("{:?} -> {:?}", path.from, path.to);
-    // }
-
-    // Recurse bellow ?
-    // -- Two lists
-    // unique_names.first().unwrap().clone()
+    println!("----");
+    println!("----");
+    println!("Valid paths found: {:?}", routes_found.len());
+    for route in (&routes_found).into_iter() {
+        println!("-- Route - Distance : {:?}", get_distance(&route));
+        for link in (&route).into_iter() {
+            print!("{:?} -> {:?}, ", link.from, link.to);
+        }
+    }
+    println!("----");
+    println!("Unique names to visit: {:?}", unique_names);
 
     return results.len();
 }
