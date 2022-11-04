7afc660d08bb2b4d5c5c471e9a97240314846585
Damian Pokorski <damianpokorski@hotmail.com>
2022-11-04 20:21:38 +0000

    2015 Day 10 Part 1

--- src/year2015/day10/mod.rs
+++ src/year2015/day10/mod.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, result};
 
 const PATH: &str = "src/year2015/day10/data.raw";
 
@@ -6,10 +6,54 @@ fn read_file() -> String {
     println!("Reading a file: {PATH}");
     return fs::read_to_string(PATH).expect("Should be able to read the file");
 }
+fn process(contents: String) -> String {
+    let characters = contents.chars();
+
+    let mut counters: Vec<(char, i32)> = vec![];
+    let mut last_counter: Option<(char, i32)> = None;
+
+    for (index, character) in characters.into_iter().enumerate() {
+        // First pass
+        if (last_counter.is_none()) {
+            last_counter = Some((character.clone(), 1));
+            counters.push(last_counter.unwrap());
+
+        // nth loop - matching counter
+        } else {
+            // Increment counter
+            if last_counter.unwrap().0.eq(&character) {
+                counters.pop();
+                last_counter = Some((last_counter.unwrap().0, last_counter.unwrap().1 + 1));
+                counters.push(last_counter.unwrap());
+            } else {
+                // Create a new counter
+                last_counter = Some((character, 1));
+                counters.push(last_counter.unwrap());
+            }
+        }
+    }
+
+    let mut result = String::from("");
+    for (character, counter) in counters {
+        result.push_str(counter.to_string().as_str());
+        result.push_str(character.to_string().as_str());
+    }
+    return result;
+}
 
 fn part1() -> usize {
-    let result = read_file();
-    return result.lines().count();
+    let contents = read_file();
+
+    let mut counter = 0;
+    let mut buffer = process(contents);
+    for i in 0..39 {
+        counter = counter + 1;
+        buffer = process(buffer);
+    }
+
+    println!("Result {:?}", buffer.len());
+    return 0;
+    // contents.lines().count();
 }
 
 fn part2() -> i32 {
