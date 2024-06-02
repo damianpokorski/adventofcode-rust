41fc64dae7f57240857a34023cf7531e9bfb0517
Damian Pokorski <damianpokorski@hotmail.com>
2024-06-02 21:42:42 +0000

    2016 Day 6 Part 2

--- src/year2016/day06/mod.rs
+++ src/year2016/day06/mod.rs
@@ -2,35 +2,35 @@ use itertools::Itertools;
 
 use crate::common::puzzle_data;
 
-fn part1() -> String {
+fn solution(ascending: bool) -> String {
     let contents = puzzle_data(std::file!());
 
     let mut most_frequent: Vec<String> = vec![];
     for index in 0..8 {
-        // Iterate through all strings -
+        // Iterate through all strings -grab nth
         let chars = contents
             .split("\n")
             .into_iter()
             .map(|line| line.get(index..index + 1).unwrap_or(&"_"))
             .collect_vec();
 
-        let sorted_by_frequency = chars
-            .clone()
+        let char_counters = chars.clone().into_iter().counts_by(|char| char);
+
+        // Order characters by frequency
+        // Part 1 = Most common
+        // Part 2 = Least common
+        let sorted_by_frequency = char_counters
             .into_iter()
-            .unique()
-            .map(|char| {
-                return (
-                    char,
-                    (chars.clone())
-                        .into_iter()
-                        .filter(|&other_char| other_char.eq(char))
-                        .count(),
-                );
+            .sorted_by(|&a, &b| {
+                return match ascending {
+                    true => a.1.cmp(&b.1),
+                    false => b.1.cmp(&a.1),
+                };
             })
-            .sorted_by(|&a, &b| b.1.cmp(&a.1))
             .map(|tuple| tuple.0)
             .collect_vec();
 
+        // Grab first
         let most_frequent_char = sorted_by_frequency.first().unwrap();
 
         // Save most frequent
@@ -39,14 +39,9 @@ fn part1() -> String {
     return most_frequent.join(&"");
 }
 
-fn part2() -> usize {
-    let contents = puzzle_data(std::file!());
-    return contents.lines().count();
-}
-
 pub fn puzzle() {
-    let part1 = part1();
-    let part2 = part2();
+    let part1 = solution(false);
+    let part2 = solution(true);
     println!("Part1: {:?}", part1);
     println!("Part2: {:?}", part2);
 }
